42e6f213e8213043c1ba4af51104a66470b518a6
c <pcsidsub@C45-SERV.upc.edu.pe>
2013-04-03 20:44:45 -0500

    arreglos 2

--- M2/ejemploArreglos2.rb
+++ M2/ejemploArreglos2.rb
@@ -19,8 +19,23 @@ total  = 0
   total / sueldos.size
 end
 
+def reporte_monto_mayor(nombres,sueldos,monto)
+  felices = Array.new
+  indice_final = sueldos.size - 1
+  for i in 0..indice_final do
+    if (sueldos[i] >= monto)
+      felices.push(nombres[i])
+    end
+  end
+  felices
+end
+
 nombres = ["Luis", "Jorge", "Lola"]
 sueldos = [1000, 2400, 5600]
+puts "El que gana mejor es: "
 puts mas_gana(nombres,sueldos)
+puts "El promedio de sueldos es: "
 puts promedio_sueldos(sueldos)
+puts "Los trabajadores que ganan arriba de 1200 son: "
+puts reporte_monto_mayor(nombres,sueldos,1200)
 
